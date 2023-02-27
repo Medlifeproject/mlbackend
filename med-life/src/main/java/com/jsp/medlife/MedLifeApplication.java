f32b93a98128745a1506b0f75b196d10991728f1
datthunaik1319 <datthunaik1319@gmail.com>
2023-02-27 19:41:27 +0530

    code

--- med-life/src/main/java/com/jsp/medlife/MedLifeApplication.java
+++ med-life/src/main/java/com/jsp/medlife/MedLifeApplication.java
@@ -8,6 +8,8 @@ public class MedLifeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedLifeApplication.class, args);
+		
+		System.out.println("med-life started...");
 	}
 
 }
